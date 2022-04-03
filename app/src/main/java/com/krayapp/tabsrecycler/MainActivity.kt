3c426d648ff65329804c7bc9deb782da63e9a8c7
januarydayfin <krayman7250@gmail.com>
2022-04-03 13:56:57 +0700

    Bottom app bar

--- app/src/main/java/com/krayapp/tabsrecycler/MainActivity.kt
+++ app/src/main/java/com/krayapp/tabsrecycler/MainActivity.kt
@@ -1,36 +1,45 @@
 package com.krayapp.tabsrecycler
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.github.terrakok.cicerone.NavigatorHolder
-import org.koin.android.ext.android.inject
 import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
+import com.krayapp.tabsrecycler.databinding.ActivityMainBinding
 import com.krayapp.tabsrecycler.view.screens.MainScreen
 import com.krayapp.tabsrecycler.view.screens.SecondScreen
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
     private val navigator = AppNavigator(this, android.R.id.content)
     private val router: Router by inject()
     private val navigationHolder: NavigatorHolder by inject()
+    private val viewBinding: ActivityMainBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         savedInstanceState ?: router.newRootScreen(MainScreen)
+        bottomMenuInit()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.bottom_menu,menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.first_page -> router.navigateTo(MainScreen)
-            R.id.second_page -> router.navigateTo(SecondScreen)
+
+    private fun bottomMenuInit() {
+        viewBinding.bottomNavigationBar.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.first_page -> {
+                    router.navigateTo(MainScreen)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.second_page -> {
+                    router.navigateTo(SecondScreen)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
         }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onResumeFragments() {
